dfb7bbfe9b30ab8a789aa4b1fe69dbded3ce1481
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-07-03 23:15:05 +0100

    Add watermarks to Pdf files.

--- pdf.py
+++ pdf.py
@@ -1,14 +1,17 @@
 import PyPDF2
-import sys
+# import sys
 
-inputs = sys.argv[1:]
 
-def pdf_combiner(pdf_list):
-	merger = PyPDF2.PdfMerger()
-	for pdf in pdf_list:
-		print(pdf)
-		merger.append(pdf)
 
-	merger.write('super.pdf')
+template = PyPDF2.PdfReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfReader(open('wtr.pdf','rb'))
+output = PyPDF2.PdfWriter()
 
-pdf_combiner(inputs)
+for i in range (template.pages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
+
